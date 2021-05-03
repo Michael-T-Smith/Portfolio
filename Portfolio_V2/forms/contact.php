160b89a7807f063fb6da98c00556c2a3c0c64c1e
Michael-T-Smith <Michael.t.smith95@gmail.com>
2021-05-03 00:12:41 -0400

    web page reworked

--- Portfolio_V2/forms/contact.php
+++ Portfolio_V2/forms/contact.php
@@ -1,16 +1,16 @@
 <?php
 
 if(isset($_POST['submit'])){
-    $to = "Michael.t.smith95@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+    $to = "Michael.t.smith95@gmail.com";
+    $from = $_POST['email'];
     $name = $_POST['name'];
-    $subject = $_POST["subject"];
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "To:" . $to;
     mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     
     }
 	
